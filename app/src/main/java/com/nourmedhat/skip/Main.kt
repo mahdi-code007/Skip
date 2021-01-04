5ccfc9cbadb903ff76174682f28202c89ea08b60
nour medhat <nouraldinmadhay@gmail.com>
2021-01-05 00:39:57 +0200

    hhhhh

--- app/src/main/java/com/nourmedhat/skip/Main.kt
+++ app/src/main/java/com/nourmedhat/skip/Main.kt
@@ -7,5 +7,6 @@ class Main : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //nour
     }
 }
